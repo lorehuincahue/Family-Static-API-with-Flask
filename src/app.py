aa309ee7fd70fe7587ca27216f6a11dc32d81310
lorehuincahue <lorehuincahue@gmail.com>
2022-12-27 20:20:19 +0000

    ok

--- src/app.py
+++ src/app.py
@@ -1,44 +1,62 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_one_member(member_id):
+    member = jackson_family.get_member(member_id)
+    return jsonify(member), 200
+
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body_last_name = request.json.get("last_name")
+    body_name = request.json.get("first_name")
+    body_age = request.json.get("age")
+    body_id = request.json.get("id")
+    body_lucky_numbers = request.json.get("lucky_numbers")
+    member = {
+        "id": body_id or jackson_family._generateId(),
+        "first_name": body_name,
+        "last_name": body_last_name,
+        "age": body_age,
+        "lucky_numbers": body_lucky_numbers,
     }
+    jackson_family.add_member(member)
+    return jsonify(None), 200
 
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_one_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
 
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,4 +1,3 @@
-
 """
 update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
@@ -6,14 +5,32 @@ update this file to implement the following already declared methods:
 - update_member: Should update a member from the self._members list
 - get_member: Should return a member from the self._members list
 """
+
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self._members = [{
+                "id": self._generateId(),
+                "first_name": "Jhon",
+                "last_name": self.last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22] },
 
-        # example list of members
-        self._members = []
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": self.last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3] },
+
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": self.last_name,
+                "age": 5,
+                "lucky_numbers": [1] }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
